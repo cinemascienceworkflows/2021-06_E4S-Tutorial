016e23ba91b75b6ca94d2ac4620de08b96a0d86f
David Honegger Rogers <dhr@lanl.gov>
2021-06-08 15:20:06 -0600

    adding exawind image

--- postprocess/post_submit.sh
+++ postprocess/post_submit.sh
@@ -11,9 +11,13 @@
 spack load paraview+python3
 
 PANTHEON_RUN_DIR=<pantheon_run_dir>
-# SCRIPT=$PANTHEON_RUN_DIR/volume_rendering_cdb_extract.py
-SCRIPT=$PANTHEON_RUN_DIR/contours_image_cdb_extract.py
+
+# uncomment one of the following lines to control the type of
+# visualization created by this script
+# VISUALIZATION=volume_rendering_cdb_extract.py
+VISUALIZATION=contours_image_cdb_extract.py
+
 INPUT=$PANTHEON_RUN_DIR
 CDB=$PANTHEON_RUN_DIR/cinema_databases/$PANTHEON_CDB
 
-pvpython --force-offscreen-rendering $SCRIPT $CDB $INPUT > pvpython.output
+pvpython --force-offscreen-rendering $PANTHEON_RUN_DIR/$VISUALIZATION $CDB $INPUT > pvpython.output
